a8b731d3b2a287a93b0ddb78ac0427350a7d9467
baseballfan <papajr@byui.edu>
2018-06-02 16:56:48 -0700

    Modified multiple files
    Changed multiple files

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,10 @@
+var cdate = new Date();
+var cdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var cmonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "november", "December"];
+var cdates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+var currentdays = cdays[cdays.getDay()];
+var currentmonth = cmonth[cdate.getMonth()];
+var currentdates = cdates[cdates.getDate()];
+
+
+document.getElementById("currentDate").innerHTML = currentdays + ", " + currentmonth + " " + currentdates + " " + cdate.getFullYear();
