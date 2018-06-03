2054dc104d455b422941c13633a7c4d7d60c3abe
baseballfan <papajr@byui.edu>
2018-06-02 19:12:59 -0700

    currentDate changes
    currentDate changes

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,15 +1,11 @@
-var cdate = new Date();
-var cdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var cmonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "november", "December"];
-var cdates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+var date = new Date();
+var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var weekDate = weekday[date.getDay()];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "november", "December"];
+var day = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 //var currentdays = cdays[cdays.getDay()];
-//var currentmonth = cmonth[cdate.getMonth()];
-//var currentdates = cdates[cdates.getDate()];
-var currentdays = cdays[cdays.getDay()] + ", " + cdates +" " + cmonth[cmonth.getMonth()] + " " + cdate.getFullYear();
-
-document.getElementById("currentdate").innerHTML=currentdays; 
+var currentMonth = month[date.getMonth()];
+var currentDate = day[date.getDate()];
 
 
-
-
-//+ ", " + currentmonth + " " + currentdates + " " + cdate.getFullYear();
+document.getElementById("currentDate").innerHTML = weekDate + ", " + currentDate + " " + currentMonth + " " + date.getFullYear();
