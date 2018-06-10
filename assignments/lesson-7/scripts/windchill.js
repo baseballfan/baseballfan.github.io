f12e8bb8f22375a2f553467a45e7b0c8d7115cb9
baseballfan <papajr@byui.edu>
2018-06-09 20:24:59 -0700

    updates

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,25 +1,22 @@
-/*windchill*/
-/*f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 */
-
 function calculatewindChill(){
 
-//get number of both beginging and ending odemeter
+
 var speedmph = document.getElementById('mphinput').value;
 var airtemp = document.getElementById('airtempinput').value;
 
-//convert what the user typed into a number.
+
 var s = parseFloat(speedmph);
 var t = parseFloat(airtemp);
 
-//figure out windchill
+
 var chilly =  35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16) 
 
-//round 2 digits
+
 var digits = 2;
 var power = Math.pow(10, digits);
 chilly = Math.round(chilly * power) / power;
 
-//Display 
+
 document.getElementById('chilly').innerHTML = chilly;
 }
 
