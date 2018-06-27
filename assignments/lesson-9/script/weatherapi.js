b10daf8192c92a72da73739df3285d34137aa07b
baseballfan <papajr@byui.edu>
2018-06-27 01:14:00 -0700

    leson 9 files
    Files and images

--- assignments/lesson-9/script/weatherapi.js
+++ assignments/lesson-9/script/weatherapi.js
@@ -1,12 +1,12 @@
-let weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4156210,us&appid=828b58bd0afbb809dc500a3b1a6a67ff&units=imperial', true);
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET','http://api.openweathermap.org/data/2.5/weather?id=4156210,us&appid=828b58bd0afbb809dc500a3b1a6a67ff&units=imperial',true);
 
-weatherRequest.responseType = 'json';
+weatherRequest.responseType = '';
 
 weatherRequest.send();
 
 weatherRequest.onload = function () {
-    let weatherInfo = JSON.parse(weatherRequest.responseText);
+  var weatherInfo = JSON.parse(weatherRequest.responseText);
     console.log(weatherInfo);
     
     document.getElementById('place').innerHTML = weatherInfo.name;
