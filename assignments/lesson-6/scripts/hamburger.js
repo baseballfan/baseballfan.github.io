cd5ef720060a997db66d4f799c80849dbd0e225a
baseballfan <papajr@byui.edu>
2018-07-17 23:27:54 -0700

    Lessons 6 & 7 updates
    Hopefully final changes

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,4 +1,9 @@
-function toggleNavMenu(){
-
-     document.getElementsByClassName("menus")[0].classList.toggle("responsive");
-}
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
